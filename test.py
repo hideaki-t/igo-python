318804e44ca48c9f2b3a1eeb6138b39667669554
Takahashi Hideaki <mymelo@gmail.com>
2010-11-23 09:15:33 +0900

    update test.

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import igo.Tagger
 
 t = igo.Tagger.Tagger('/mnt/dev/ipadic')
-l = t.parse(u'こんにちは世界')
-for m in l:
+for m in t.parse(u'こんにちは世界'):
+	print m.surface, m.feature, m.start
+for m in t.parse(u'私の名前は中野です。'):
 	print m.surface, m.feature, m.start
