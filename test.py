0636186832cce2e439443bd6731a1a99b2cbfba9
Takahashi Hideaki <mymelo@gmail.com>
2010-11-25 18:13:00 +0900

    checked source style with pep8

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import igo.Tagger
 
 t = igo.Tagger.Tagger('/mnt/dev/ipadic_gae', gae=True)
 for m in t.parse(u'私の名前は中野です。'):
-	print m.surface, m.feature, m.start
+    print m.surface, m.feature, m.start
 print
 t = igo.Tagger.Tagger('/mnt/dev/ipadic')
 for m in t.parse(u'こんにちは世界'):
-	print m.surface, m.feature, m.start
+    print m.surface, m.feature, m.start
