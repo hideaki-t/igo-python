31dcf3795d94bb879f923593c416a261c5a9c470
Takahashi Hideaki <mymelo@gmail.com>
2010-11-27 02:19:14 +0900

    remove unsed comment

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 import igo.Tagger
 
-t = igo.Tagger.Tagger('../../ipadic', gae=True)
-#t = igo.Tagger.Tagger('/mnt/dev/ipadic_gae', gae=True)
+t = igo.Tagger.Tagger('/mnt/dev/ipadic_gae', gae=True)
 for m in t.parse(u'私の名前は中野です。'):
     print m.surface, m.feature, m.start
 print
