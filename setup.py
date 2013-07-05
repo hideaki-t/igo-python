a77f3f0c29ed0069e1597027f3dd297915f1fdc1
Hideaki Takahashi <mymelo@gmail.com>
2013-07-05 14:43:21 -0400

    release 0.9.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='igo-python',
-    version='0.9.2',
+    version='0.9.3',
     description='Python port of Igo Japanese morphological analyzer',
     long_description=open('README').read() + "\n\n" + open('CHANGES').read(),
     author='Hideaki Takahashi',
@@ -19,10 +19,9 @@ setup(
                  'Operating System :: OS Independent',
                  'Operating System :: Microsoft :: Windows',
                  'Operating System :: POSIX :: Linux',
-                 'Programming Language :: Python :: 2.5',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
                  'Topic :: Scientific/Engineering :: Information Analysis',
                  'Topic :: Software Development :: Libraries :: Python Modules',
                  'Topic :: Text Processing :: Linguistic',
